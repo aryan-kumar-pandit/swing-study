52351c9f4a253f4bd30cdc4bce845ff7281634b5
aryan kumar <aryankumarpandito@gmail.com>
2024-05-02 20:54:07 +0530

    2nd commit1

--- Swing study/src/com/swing1/JFrame1.java
+++ Swing study/src/com/swing1/JFrame1.java
@@ -5,6 +5,7 @@ import java.awt.Container;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class JFrame1 {
 
@@ -13,16 +14,13 @@ public class JFrame1 {
 		JFrame frame=new JFrame();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.setSize(700, 500);
-		//frame.setLocation(100, 100);
 		frame.setTitle("Aryan 1st swing");
-		ImageIcon icon=new ImageIcon("C:\\Users\\Aryan\\eclipse-workspace\\Swing study\\src\\com\\swing1\\abc.jpg");
-		frame.setIconImage(icon.getImage());
 		frame.setBounds(100, 100, 1000, 500);
 		Container c=frame.getContentPane();
-		c.setBackground(Color.red);
-		//Color color = new Color();
-		//frame.setResizable(false); -- frame wont' get resized
+		c.setLayout(null);
+		JLabel label=new JLabel("username");
+		label.setBounds(100,50,100,30);
+		c.add(label);
 		
 		
 
